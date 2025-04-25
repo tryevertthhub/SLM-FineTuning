1f624f2993cb431ac5821a5f06d5705cbc159649
tryevertthhub <tryevertth@gmail.com>
2025-04-24 21:56:15 -0800

    feat: add config & dataloader

--- main.py
+++ main.py
@@ -18,3 +18,19 @@ import numpy as np
 from huggingface_hub import interpreter_login
 
 # interpreter_login()
+
+
+import os
+# disable Weights and Biases
+os.environ['WANDB_DISABLED']="true"
+
+huggingface_dataset_name = "neil-code/dialogsum-test"
+dataset = load_dataset(huggingface_dataset_name)
+
+compute_dtype = getattr(torch, "float16")
+bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=False,
+    )
