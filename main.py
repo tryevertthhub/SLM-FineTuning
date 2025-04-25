609c889002edd956f8982e32cf179d73d790caa1
tryevertthhub <tryevertth@gmail.com>
2025-04-24 21:53:19 -0800

    feat: add library

---
+++ main.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+    GenerationConfig
+)
+from tqdm import tqdm
+from trl import SFTTrainer
+import torch
+import time
+import pandas as pd
+import numpy as np
+from huggingface_hub import interpreter_login
+
+# interpreter_login()
