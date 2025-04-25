f56d703384c20d6a2e98d6de560d26bfde0545ce
tryevertthhub <tryevertth@gmail.com>
2025-04-24 23:25:39 -0800

    feat: add preprocess_dataset function

--- main.py
+++ main.py
@@ -110,3 +110,29 @@ def preprocess_batch(batch, tokenizer, max_length):
         max_length=max_length,
         truncation=True,
     )
+
+def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int,seed, dataset):
+    """Format & tokenize it so it is ready for training
+    :param tokenizer (AutoTokenizer): Model Tokenizer
+    :param max_length (int): Maximum number of tokens to emit from tokenizer
+    """
+
+    # Add prompt to each sample
+    print("Preprocessing dataset...")
+    dataset=dataset.map(create_prompt_formats,batched=True)
+
+    # Apply preprocessing to each batch of the dataset & and remove 'instruction', 'context', 'response', 'category' fields
+    _preprocessing_function = partial(preprocess_batch, max_length=max_length, tokenizer=tokenizer)
+    dataset = dataset.map(
+        _preprocessing_function,
+        batched=True,
+        remove_columns=['id', 'topic', 'dialogue', 'summary'],
+    )
+
+    # Filter out samples that have input_ids exceeding max_length
+    dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+    
+    # Shuffle dataset
+    dataset = dataset.shuffle(seed=seed)
+
+    return dataset
